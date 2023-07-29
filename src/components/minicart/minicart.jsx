19125b3306c771cea016a5325e37846aad29425c
LorranSilva <lorran.tito@outlook.com>
2023-07-29 17:31:41 -0300

    FEAT: calcule cep value

--- src/components/minicart/minicart.jsx
+++ src/components/minicart/minicart.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './minicart.scss';
 
+const MiniCart = ({ cartItems: initialCartItems }) => {
+  const [cartItems, setCartItems] = useState(initialCartItems);
 
-const MiniCart = ({ cartItems }) => {
+  useEffect(() => {
+    // Recuperar o carrinho do localStorage ao montar o componente
+    const savedCartItems = JSON.parse(localStorage.getItem('cartItems'));
+    if (savedCartItems) {
+      setCartItems(savedCartItems);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Salvar o carrinho no localStorage sempre que ele for atualizado
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
+  
+  const [cep, setCep] = useState('');
+  const [frete, setFrete] = useState(null);
 
   const calculateTotalPrice = () => {
     let totalPrice = 0;
@@ -11,6 +28,21 @@ const MiniCart = ({ cartItems }) => {
       totalPrice += parseFloat(cartItem.price);
     });
     return totalPrice;
+  };
+
+  const handleCepChange = (event) => {
+    setCep(event.target.value);
+  };
+
+  const handleCalculateFrete = async () => {
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      console.log(response.data)
+      setFrete(response.data);
+    } catch (error) {
+      console.error(error);
+      setFrete(null);
+    }
   };
 
   return (
@@ -30,12 +62,22 @@ const MiniCart = ({ cartItems }) => {
               })}
             </ul>
             <div>
-            {Object.keys(cartItems).length >= 1 && (
-              <div className="total-price">
-                <p>Total do Carrinho: R${calculateTotalPrice().toFixed(2)}</p>
-              </div>
-            )}
-          </div>
+              {Object.keys(cartItems).length >= 1 && (
+                <div className="total-price">
+                  <p>Total do Carrinho: R${calculateTotalPrice().toFixed(2)}</p>
+                  <input
+                    type="text"
+                    value={cep}
+                    onChange={handleCepChange}
+                    placeholder="Digite o CEP"
+                  />
+                  <button onClick={handleCalculateFrete}>Calcular Frete</button>
+                  {frete && (
+                    <p>Valor do Frete: Grátis</p>
+                  )}
+                </div>
+              )}
+            </div>
           </>
         ) : (
           <p>O carrinho está vazio.</p>
